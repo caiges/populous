345b69fa333ab1562223e2351725c30fa7e2cd94
Weston Nielson <wnielson@media.ucla.edu>
2008-10-30 07:05:42 +0000

    Updated default inlines to use new form handling.
    git-svn-id: http://code.populousproject.com/svn/trunk@30 2d015e74-dcdc-4604-a43e-ef45d21546f3

--- populous/inlines/inlines.py
+++ populous/inlines/inlines.py
@@ -7,6 +7,12 @@ from django.utils.translation import ugettext_lazy as _
 from populous.inlines.base import Inline
 from populous.inlines.forms import InlineForm
 
+
+class TextInlineForm(InlineForm):
+    title = forms.CharField(label=_("Title"), required=False)
+    content = forms.CharField(label=_("Content"), widget=forms.Textarea)
+    template = forms.CharField(label=_("Template"), required=False)
+
 class TextInline(Inline):
     """
     A simple inline that stores text.  A custom form can be defined as well. 
@@ -14,12 +20,12 @@ class TextInline(Inline):
     default_template = 'inlines/textinline.html'
     verbose_name = _('text box')
     verbose_name_plural = _('text boxes')
-    
-    class AdminForm(InlineForm):
-        title = forms.CharField(label=_("Title"), required=False)
-        content = forms.CharField(label=_("Content"), widget=forms.Textarea)
-        template = forms.CharField(label=_("Template"), required=False)
+    form = TextInlineForm
 
+
+class TemplateInlineForm(InlineForm):
+    template = forms.CharField(label=_("Template"), required=False)
+        
 class TemplateInline(Inline):
     """
     An inline that doesn't have any content, but will include a template specific
@@ -27,10 +33,14 @@ class TemplateInline(Inline):
     this inline will get rendered in the same context at the referencing object.
     """
     verbose_name = _('template')
+    form = TemplateInlineForm
     # TODO: think more about this one.
-    class AdminForm(InlineForm):
-        template = forms.CharField(label=_("Template"), required=False)
 
+
+class GenericModelInlineForm(InlineForm):
+    #content_type = forms.ModelChoiceField
+    #object_id = forms.IntegerField()
+    pass
 
 class GenericModelInline(Inline):
     """
@@ -38,12 +48,12 @@ class GenericModelInline(Inline):
     application.  This allows...
     """
     verbose_name = _('generic model')
-    class AdminForm(InlineForm):
-        #content_type = forms.ModelChoiceField
-        #object_id = forms.IntegerField()
-        pass
+    form = GenericModelInlineForm
+
+
+class ReverseInlineForm(InlineForm):
+    pass
 
 class ReverseInline(Inline):
     verbose_name = _('reverse inline')
-    class AdminForm(InlineForm):
-        pass
+    form = ReverseInlineForm
