3bb3c1bd58aef49f24c8e3b1603bd5aa3e7988e5
Weston Nielson <wnielson@media.ucla.edu>
2008-11-24 20:06:03 +0000

    Temporarily commented out IM related code in messaging.
    git-svn-id: http://code.populousproject.com/svn/trunk@93 2d015e74-dcdc-4604-a43e-ef45d21546f3

--- populous/messaging/__init__.py
+++ populous/messaging/__init__.py
@@ -1,6 +1,6 @@
-from populous.messaging.protocols.im import IMMessage, IMConnection
+#from populous.messaging.protocols.im import IMMessage, IMConnection
 from populous.messaging.protocols.sms import SMSMessage
-from django.core.mail import Emailmessage
+from django.core.mail import EmailMessage
 
 # Convenient alias
-im_connection = IMConnection()
+#im_connection = IMConnection()
